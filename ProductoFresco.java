8a45250e30093ed549f1e268c5c47fde9c0368d6
milirope <64318749+milirope@users.noreply.github.com>
2021-10-20 20:30:36 -0400

    Create ProductoFresco.java

---
+++ ProductoFresco.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.poo;
+
+public class ProductoFresco extends Producto{
+    
+    public ProductoFresco(String fechaCaducidad, Number numeroLote, String fechaEnvasado, String paisOrigen) {
+        super(fechaCaducidad, numeroLote, fechaEnvasado, paisOrigen);
+    }
+
+    
+}
